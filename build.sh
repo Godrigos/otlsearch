7b876b1d1b0cd271b6482ad2b055236c7417365e
Rodrigo Aluizio <r.aluizio@gmail.com>
2021-12-22 09:15:19 -0300

    Modified build system.

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Linux compilation." 
+cd Linux
+
+if [ -d "build"]; then
+  echo "build directory already exists."
+else
+  mkdir build
+fi
+
+cd build
+conan install .. --build=missing
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake --build . --clean-first
+
+echo "Linux compilation ended."
+echo
+
+echo "Windows cross compilation"
+cd ../../Windows
+
+if [ -d "build"]; then
+  echo "build directory already exists."
+else
+  mkdir build
+fi
+
+cd build
+conan install .. --build=missing -pr:h windows -pr:b default
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . --clean-first
+
+cd ../..
+
+echo "Windows cross compilation ended."
+
+# Attention to conan profiles when compiling or cross compiling
